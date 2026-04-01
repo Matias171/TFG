ff087cfb372344fa647e1837d16b3336d8ff59b5
arich <arich@Ari>
2026-04-01 18:49:49 +0200

    Subiendo proyecto TFG

---
+++ src/main/java/com/viajes/viajesapp/ViajesappApplication.java
@@ -0,0 +1,13 @@
+package com.viajes.viajesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajesappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viajes/viajesapp/ViajesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viajes.viajesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
