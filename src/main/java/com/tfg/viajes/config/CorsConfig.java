8301280f039073920492d4978a5af2fbe3fd3eed
Matias171 <matias-lema@hotmail.com>
2026-06-16 23:46:27 +0200

    backend nuevo CorsConfig

--- src/main/java/com/tfg/viajes/config/CorsConfig.java
+++ src/main/java/com/tfg/viajes/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/api/**")
 		        .allowedOrigins(
 		                "http://localhost:4200",           // desarrollo local
-		                "https://viajesapp-frontend.vercel.app"    // producción Vercel (cambia esto después)
+		                "viajesapp-frontend.vercel.app"    // producción Vercel (cambia esto después)
 		            )
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
